028c3c6f83cffb3a95ab3a7ad29c25158aa2a72a
Thomas-Lim <52769104+Thomas-Lim@users.noreply.github.com>
2020-11-03 23:31:37 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// println은 줄바꿈이 적용된 것
+		System.out.println("줄바꿈이 적용 된 것"); 
+		
+		// print는 줄바꿈이 적용되지 않은 것
+		System.out.print("줄바꿈이 적용되지 않음");
+	}
+
+}
