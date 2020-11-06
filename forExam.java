665eef6b8dd9a3bb8b37c8bcb03c67c8d8a5ee48
Thomas-Lim <52769104+Thomas-Lim@users.noreply.github.com>
2020-11-06 22:48:10 +0900

    Add files via upload

---
+++ forExam.java
@@ -0,0 +1,23 @@
+
+public class forExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // 배열의 시작
+		String str[] = {"하나", "둘", "셋"}; // 중괄호로 열어줌에 주의.
+		
+		for(int i=0; i<str.length; i++) { // str.length;는 배열의 길이 = 갯수 = 3
+			System.out.println("배열 포문");
+		}
+			
+		
+		for(int i=0; i<3; i++) { // 3보다 작을 떄 까지만 실행. 3보다 작을 때 까지만 결과 취합.
+			System.out.println("가나다라");
+		}
+		
+		System.out.println("마바사아.");
+		
+		
+	}
+
+}
