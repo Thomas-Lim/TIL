6aadf2780a042ddaeca179cb63dd89230174ac21
Thomas-Lim <52769104+Thomas-Lim@users.noreply.github.com>
2020-11-07 23:53:45 +0900

    Add files via upload
    # while 반복문 
    
    ## Scanner는 숫자나 문자 등을 입력받을 수 있도록 함
    		Scanner sc = new Scanner(System.in);

---
+++ whileExam.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class whileExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Scanner는 숫자나 문자 등을 입력받을 수 있도록 함
+		Scanner sc = new Scanner(System.in);  
+		
+		int i = sc.nextInt(); // 변수.nextInt(); 로 숫자값 입력 가능
+		
+		// int i = 0;         // 0으로 시작
+		while(i<10) {      // 10보다 작을 때 까지 넣고
+			System.out.println(i);   //10보다 작을 떄 까지 하나씩 출력함.
+			i++;          // 그리고 증가
+		}
+	}
+
+}
